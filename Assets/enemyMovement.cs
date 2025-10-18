71646450b928c00ab39579ab7114b3b73aa0ab11
ocfrey <ocfrey270@gmail.com>
2025-10-17 20:25:23 -0600

    enemy movement scripts

---
+++ Assets/enemyMovement.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    public float startMovingThreshold;
+    public float stopMovingThreshold;
+    public float velocity;
+    private Transform player;
+    private Transform enemy;
+    private Rigidbody2D rb;
+    private float distanceFromPlayer;
+    private float absoluteDistanceFromPlayer;
+
+    
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        enemy = GetComponent<Transform>();
+        player = FindAnyObjectByType<Player>().transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        distanceFromPlayer = enemy.position.x - player.position.x;
+        absoluteDistanceFromPlayer = Math.Abs(distanceFromPlayer);
+        if (absoluteDistanceFromPlayer < startMovingThreshold)
+        {
+            if (absoluteDistanceFromPlayer < stopMovingThreshold)
+            {
+                rb.linearVelocityX = 0;
+            }
+            else if (distanceFromPlayer > 0)
+            {
+                rb.linearVelocityX = -velocity;
+            }
+            else if (distanceFromPlayer < 0)
+            {
+                rb.linearVelocityX = velocity;
+            }
+            Debug.Log(absoluteDistanceFromPlayer);
+        }
+    }
+}
