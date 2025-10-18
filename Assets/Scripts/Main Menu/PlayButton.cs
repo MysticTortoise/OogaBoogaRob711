283701abb7fe0fe4f03a1ef5604fa8a380c4d591
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-10-17 23:33:50 -0600

    exit button completed

---
+++ Assets/Scripts/Main Menu/PlayButton.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    [SerializeField] Button playButton;
+    [SerializeField] TextMeshProUGUI playButtonText;
+
+    
+
+}
