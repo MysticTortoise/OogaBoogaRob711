f04cd04275decb8adb855e3f1f2533218f3fc3ab
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-10-17 19:51:04 -0600

    more folders and menu progress

---
+++ Assets/Scripts/Main Menu/ExitButton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ExitButton : MonoBehaviour
+{
+    bool confirmExit = false;
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
