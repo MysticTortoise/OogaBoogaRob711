9e6e8359043ba2969a9f544d4445711f2131d6d1
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-10-18 20:39:57 -0600

    requested push

--- Assets/Scripts/PlayerUI/ItemDisplaySingle.cs
+++ Assets/Scripts/PlayerUI/ItemDisplaySingle.cs
@@ -12,12 +12,27 @@ public class ItemDisplaySingle : MonoBehaviour
 
     public void StartItemCooldown(float cooldownTime)
     {
+        DOTween.Kill(cooldownImage);
+        cooldownCompleteEffect.color = new Color(255, 255, 255, 175);
+        cooldownCompleteEffect.rectTransform.localScale = Vector3.one;
+        cooldownCompleteEffect.gameObject.SetActive(true);
+
         StartCoroutine(PlayCooldownEffect(cooldownTime));
     }
 
     IEnumerator PlayCooldownEffect(float cooldownTime)
     {
-        
+        DOTween.Sequence()
+            .Append(cooldownImage.DOFillAmount(0, cooldownTime))
+            .Join(cooldownImage.DOColor(new Color(255, 255, 255, 0), cooldownTime)
+            .SetEase(Ease.Linear));
+        yield return DOTween.Sequence().WaitForCompletion();
+        DOTween.Sequence()
+            .Append(cooldownCompleteEffect.DOFade(0, 0.3f))
+            .Join(cooldownCompleteEffect.rectTransform.DOScale(1.5f, 0.3f))
+            .SetEase(Ease.OutQuint);
+        yield return DOTween.Sequence().WaitForCompletion();
+        cooldownCompleteEffect.gameObject.SetActive(false);
     }
 
 }
