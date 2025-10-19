f74b8d1252a9d1b12d745d90c935da82c431d1b1
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-10-19 12:04:16 -0600

    level select menu complete

--- Assets/Scripts/Level Select/LevelSelectButton.cs
+++ Assets/Scripts/Level Select/LevelSelectButton.cs
@@ -11,6 +11,9 @@ public class LevelSelectButton : MonoBehaviour
     [SerializeField] Button levelButton;
     [SerializeField] TextMeshProUGUI levelButtonText;
     [SerializeField] RectTransform textFinalPos;
+    [SerializeField] RectTransform moveToCenterPos;
+    [SerializeField] Image fadeToWhite;
+    [SerializeField] AudioSource enterLevelSFX;
     [SerializeField] string levelToLoad;
 
     bool lockActions = false; // prevent all buttons from being interacted with
@@ -18,6 +21,7 @@ public class LevelSelectButton : MonoBehaviour
     public void LockActions()
     {
         lockActions = true;
+        levelButton.interactable = false;
     }
 
     public void OnHover()
@@ -27,7 +31,15 @@ public class LevelSelectButton : MonoBehaviour
 
         DOTween.Kill(levelButtonText);
         DOTween.Kill(levelButton);
-        
+
+        levelButtonText.rectTransform.localScale = new Vector2(1.5f, 1.5f);
+        levelButtonText.DOFade(0f, 0f);
+
+        levelButtonText.gameObject.SetActive(true);
+        levelButtonText.rectTransform.DOMove(textFinalPos.position, 0.2f).SetEase(Ease.OutQuint);
+        levelButtonText.rectTransform.DOScale(1f, 0.2f).SetEase(Ease.OutQuint);
+        levelButtonText.DOFade(1f, 0.2f).SetEase(Ease.OutQuint);
+
         levelButton.transform.DOScale(1.15f, 0.2f).SetEase(Ease.OutQuint);
     }
 
@@ -39,7 +51,14 @@ public class LevelSelectButton : MonoBehaviour
         DOTween.Kill(levelButtonText);
         DOTween.Kill(levelButton);
 
-        levelButton.transform.DOScale(1f, 0.2f).SetEase(Ease.OutQuint);
+        levelButtonText.rectTransform.DOMove(levelButton.image.rectTransform.position, 0.2f).SetEase(Ease.OutQuint);
+        levelButtonText.rectTransform.DOScale(1.5f, 0.2f).SetEase(Ease.OutQuint);
+        levelButtonText.DOFade(0f, 0.2f).SetEase(Ease.OutQuint);
+
+        levelButton.transform.DOScale(1f, 0.2f).SetEase(Ease.OutQuint).OnComplete(() =>
+        {
+            levelButtonText.gameObject.SetActive(false);
+        });
     }
 
     public void OnClick()
@@ -58,6 +77,26 @@ public class LevelSelectButton : MonoBehaviour
 
     IEnumerator StartTransitionSequence()
     {
-        yield return null;
+        fadeToWhite.gameObject.SetActive(true);
+
+        // button expands and goes back
+        DOTween.Sequence()
+            .Append(levelButton.transform.DOScale(1.7f, 0.1f).SetEase(Ease.OutQuint))
+            .Append(levelButton.transform.DOScale(1.3f, 0.9f).SetEase(Ease.OutQuint));
+
+        // text pops out and fades away
+        yield return DOTween.Sequence()
+            .Append(levelButtonText.rectTransform.DOScale(1.5f, 0.3f).SetEase(Ease.OutQuint))
+            .Join(levelButtonText.DOFade(0f, 0.3f).SetEase(Ease.OutQuint))
+            .WaitForCompletion();
+
+        // make button go to center and fade white
+        enterLevelSFX.Play();
+        yield return DOTween.Sequence()
+            .Append(levelButton.transform.DOMove(moveToCenterPos.position, 4f).SetEase(Ease.OutQuad))
+            .Join(fadeToWhite.DOFade(1f, 4f).SetEase(Ease.Linear))
+            .WaitForCompletion();
+
+        SceneManager.LoadScene(levelToLoad);
     }
 }
